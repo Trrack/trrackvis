6d8f2d953f23ef5e004774964702e67e95ada80b
Kiran Gadhave <kirangadhave2@gmail.com>
2023-01-26 15:30:20 -0700

    build(react): added release config for semantic-release-plus

---
+++ packages/react/release.config.js
@@ -0,0 +1,30 @@
+const name = require('./package.json').name;
+const libraryFolderName = 'react';
+const srcRoot = `packages/${libraryFolderName}`;
+
+module.exports = {
+    extends: 'release.config.base.js',
+    pkgRoot: `dist/${srcRoot}`,
+    tagFromat: name + '@${version}',
+    commitPaths: [`${srcRoot}/*`],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: `${srcRoot}/CHANGELOG.md`,
+            },
+        ],
+        '@semantic-release/npm',
+        [
+            '@semantic-release/git',
+            {
+                assets: [`${srcRoot}/package.json`, `${srcRoot}/CHANGELOG.md`],
+                message:
+                    `release(version): Release ${name} ` +
+                    '${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+    ],
+};
