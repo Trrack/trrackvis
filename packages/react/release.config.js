298d22137c808249eb8b7c67468ac26e41747006
Kiran Gadhave <kirangadhave2@gmail.com>
2023-01-26 15:57:42 -0700

    chore: testing semantic-release

--- packages/react/release.config.js
+++ packages/react/release.config.js
@@ -4,6 +4,14 @@ const srcRoot = `packages/${libraryFolderName}`;
 
 module.exports = {
     extends: 'release.config.base.js',
+    branches: [
+        '+([0-9])?(.{+([0-9]),x}).x',
+        'master',
+        'next',
+        'next-major',
+        { name: 'beta', prerelease: true },
+        { name: 'alpha', prerelease: true },
+    ],
     pkgRoot: `dist/${srcRoot}`,
     tagFromat: name + '@${version}',
     commitPaths: [`${srcRoot}/*`],
