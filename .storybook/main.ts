de5076a4e677fb8629625aacb14e00af06f776a5
Kiran Gadhave <kirangadhave2@gmail.com>
2023-03-10 11:43:55 -0700

    fix(react): fixed storybook config after update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,7 @@
 import type { StorybookConfig } from '@storybook/core-common';
 
 export const rootMain: StorybookConfig = {
+  stories: [],
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
   //   // Return the altered config
