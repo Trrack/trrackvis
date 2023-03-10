f739de374f7b62a4a04c53ece3d50f53d38ece3b
Kiran Gadhave <kirangadhave2@gmail.com>
2023-03-10 12:29:43 -0700

    fix(react): made config option in ProvVisCreator optional

--- packages/react/src/components/ProvVisCreator.tsx
+++ packages/react/src/components/ProvVisCreator.tsx
@@ -1,6 +1,9 @@
 import { initializeTrrack, NodeId } from '@trrack/core';
 import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { ProvVis, ProvVisConfig } from './ProvVis';
+
+console.log(createRoot);
 
 export function ProvVisCreator<
     TrrackInstance extends ReturnType<typeof initializeTrrack> = ReturnType<
@@ -9,21 +12,27 @@ export function ProvVisCreator<
 >(
     node: Element,
     trrackInstance: TrrackInstance,
-    config: Partial<ProvVisConfig<any, any, any>>
+    config: Partial<ProvVisConfig<any, any, any>> = {}
 ) {
+    const vis = (
+        <ProvVis
+            root={trrackInstance.root.id}
+            config={{
+                changeCurrent: (id: NodeId) => trrackInstance.to(id),
+                ...config,
+            }}
+            nodeMap={trrackInstance.graph.backend.nodes}
+            currentNode={trrackInstance.current.id}
+        />
+    );
+
     function renderTrrack() {
-        render(
-            <ProvVis
-                root={trrackInstance.root.id}
-                config={{
-                    changeCurrent: (id: NodeId) => trrackInstance.to(id),
-                    ...config,
-                }}
-                nodeMap={trrackInstance.graph.backend.nodes}
-                currentNode={trrackInstance.current.id}
-            />,
-            node
-        );
+        if (createRoot) {
+            const root = createRoot(node);
+            root.render(vis);
+        } else {
+            render(vis, node);
+        }
     }
 
     trrackInstance.currentChange(() => {
